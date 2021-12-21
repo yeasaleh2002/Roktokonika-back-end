38344b3143a443e9e628916b1593f5504bdc34c9
unknown <yeasalehofficial2002@gmail.com>
2021-12-21 16:44:56 +0600

    testt

--- index.js
+++ index.js
@@ -126,6 +126,8 @@ async function run() {
         res.send(posts);
     })
 
+      
+
       // ----------- delete post--------------
       app.delete('/posts/:id', async (req, res) => {
         const id = req.params.id;
